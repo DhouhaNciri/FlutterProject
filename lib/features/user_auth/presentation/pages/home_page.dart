9450ff79d86f89a65aec5fd1ebe733831fb307e6
DhouhaNciri <dhouha.nciri123@gmail.com>
2024-05-07 21:58:49 +0100

    mobile

--- lib/features/user_auth/presentation/pages/home_page.dart
+++ lib/features/user_auth/presentation/pages/home_page.dart
@@ -115,79 +115,80 @@ import 'package:flutter_app/features/user_auth/presentation/pages/profile_page.d
 import 'package:flutter_app/features/user_auth/presentation/pages/setting_page.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final FirebaseAuth _auth = FirebaseAuth.instance;
     final GoogleSignIn googleSignIn = GoogleSignIn();
 
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-        ),
-        drawer: Drawer(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      drawer: Drawer(
 
-          // Utilisez le thème sombre si activé
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color:  Color.fromARGB(255, 5, 120, 120),
-                ),
-                child: Image.asset(
-                  'images/TunisiaApp.jpg',
-                  width: 1500,
-                  height: 1500,
-                  fit: BoxFit.cover,
-                ),
+        // Utilisez le thème sombre si activé
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color:  Color.fromARGB(255, 5, 120, 120),
               ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Setting'),
-                onTap: () =>
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => SettingsPage())),
+              child: Image.asset(
+                'images/TunisiaApp.jpg',
+                width: 1500,
+                height: 1500,
+                fit: BoxFit.cover,
               ),
-              ListTile(
-                leading: Icon(Icons.person), // Icône Profile
-                title: Text('Profile'),
-                onTap: () =>
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => ProfilePage())),
-              ),
-              ListTile(
-                leading: Icon(Icons.info), // Icône About
-                title: Text('About'),
-                onTap: () =>
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => AboutPage())),
-              ),
-              ListTile(
-                leading: Icon(Icons.logout), // Icône Logout
-                title: Text('Logout'),
-                onTap: () async {
-                  // await FirebaseAuth.instance.signOut();
-                  await _auth.signOut();
-                  // await googleSignIn.disconnect();
-                  Navigator.of(context)
-                      .push(
-                      MaterialPageRoute(builder: (context) => LoginPage()));
-                },
-              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Setting'),
+              onTap: () =>
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => SettingsPage())),
+            ),
+            ListTile(
+              leading: Icon(Icons.person), // Icône Profile
+              title: Text('Profile'),
+              onTap: () =>
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ProfilePage())),
+            ),
+            ListTile(
+              leading: Icon(Icons.info), // Icône About
+              title: Text('About'),
+              onTap: () =>
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => AboutPage())),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout), // Icône Logout
+              title: Text('Logout'),
+              onTap: () async {
+                //await FirebaseAuth.instance.signOut();
+                await _auth.signOut();
+                // await googleSignIn.disconnect();
+                Navigator.of(context)
+                    .push(
+                    MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+            ),
 
-            ],
-          ),
+          ],
         ),
-        body: Container(
-        
-          child: Center(
-            child: Text('This is the home Page'),
-          ),
-        
-        
+      ),
+      body: Container(
+
+        child: Center(
+          child: Text('This is the home Page'),
         ),
-      );
-    }
+
+        //
+      ),
+    );
   }
+}
 
